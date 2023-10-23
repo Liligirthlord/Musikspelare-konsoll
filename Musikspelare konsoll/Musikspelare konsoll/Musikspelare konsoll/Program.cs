e4f8292b8681621dd352ad600c542da55d413157
Liligirthlord <70008350+Liligirthlord@users.noreply.github.com>
2023-10-23 12:03:35 +0200

    Add files via upload

---
+++ Musikspelare konsoll/Musikspelare konsoll/Musikspelare konsoll/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WMPLib;
+
+WindowsMediaPlayer spelare = new WindowsMediaPlayer();
+
+string[] genre = { "Jazz", "Funk" };
+string[,] låtar = { { "Autumn Leaves", "Take Five" }, { "Just Around The Corner", "Come On Come Over" } };
+
+Console.WriteLine("Välkommen till Alexanders musikspelare!");
+Console.WriteLine("Du kommer att få välja mellan två genre där respektive genre har 2 låtar att välja mellan");
+Console.WriteLine("Tryck på enter för att starta...");
+var start = Console.ReadKey();
+
+while (start.Key != ConsoleKey.Enter)
+{
+    Console.Clear();
+    Console.WriteLine("Du behöver trycka på enter för att starta...");
+    start = Console.ReadKey();
+}
+if (start.Key == ConsoleKey.Enter )
+{
+    Console.Clear();
+    Console.WriteLine("Skriv vilken av följande genre vill du lyssna på: ");
+    Console.WriteLine($"\n{genre[0]}\t{genre[1]}" );
+    string genreVal = Console.ReadLine();
+}
+
+
+
+
